e03f21ce4c78fe4d5f4a4236097d5b0a0bec073d
rawnullbyte <120054040+rawnullbyte@users.noreply.github.com>
2025-11-21 20:00:49 +0100

    Enable RGB_MATRIX_CUSTOM_USER in rules.mk
    Enable RGB matrix customization for the K8 Pro.

--- keyboards/keychron/k8_pro/rules.mk
+++ keyboards/keychron/k8_pro/rules.mk
@@ -1,7 +1,6 @@
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 OPT_DEFS += -DNO_USB_STARTUP_CHECK -DENABLE_FACTORY_TEST
-RGB_MATRIX_CUSTOM_USER = yes
 
 SRC += matrix.c
 
